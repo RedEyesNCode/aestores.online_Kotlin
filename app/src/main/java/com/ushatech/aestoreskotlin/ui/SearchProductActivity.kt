0ea68124d7e386c742425b88c79f02ad23e0b04f
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2023-02-17 18:35:49 +0530

    Update for search activity and looking for recycler inside recycler view in kotlin

--- app/src/main/java/com/ushatech/aestoreskotlin/ui/SearchProductActivity.kt
+++ app/src/main/java/com/ushatech/aestoreskotlin/ui/SearchProductActivity.kt
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ushatech.aestoreskotlin.R
 import com.ushatech.aestoreskotlin.base.BaseActivity
+import com.ushatech.aestoreskotlin.databinding.ActivitySearchProductBinding
 
 class SearchProductActivity : BaseActivity() {
+
+    private lateinit var binding:ActivitySearchProductBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_product)
+        binding = ActivitySearchProductBinding.inflate(layoutInflater)
+        binding.ivClose.setOnClickListener {
+            finish()
+        }
+        setContentView(binding.root)
     }
 }
