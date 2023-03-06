a2e7ff529be131d6446c867ea8b6201846c3d13a
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2023-03-06 15:42:41 +0530

    Update design for home_side_menu for category text

--- app/src/main/java/com/ushatech/aestoreskotlin/session/Constant.kt
+++ app/src/main/java/com/ushatech/aestoreskotlin/session/Constant.kt
@@ -2,13 +2,13 @@ package com.ushatech.aestoreskotlin.session
 
 object Constant {
 
-    val OOPS_SW: String = "Oops something went wrong !"
-    val PREFERENCES_NAME: String = "TRUE_CLUB_FINTECH"
-    const val DEV_ASHUTOSH: String = "DEV_ASHUTOSH"
-    const val GOOGLE_BASE_URL = "REQUIRED"
+        val OOPS_SW: String = "Oops something went wrong !"
+        val PREFERENCES_NAME: String = "TRUE_CLUB_FINTECH"
+        const val DEV_ASHUTOSH: String = "DEV_ASHUTOSH"
+        const val GOOGLE_BASE_URL = "REQUIRED"
 
-    const val USER_ID:String = "USER_ID"
-    const val IS_LOGGED_IN:String = "IS_LOGGED_IN"
-    const val USER_INFO = "USER_INFO"
+        const val USER_ID:String = "USER_ID"
+        const val IS_LOGGED_IN:String = "IS_LOGGED_IN"
+        const val USER_INFO = "USER_INFO"
 
 }
