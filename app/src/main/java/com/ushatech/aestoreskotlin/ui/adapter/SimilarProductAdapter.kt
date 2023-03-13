a590f3ea0a153ea6c6f36174fb027a81c785897c
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2023-03-13 13:15:51 +0530

    Make design for similar products

---
+++ app/src/main/java/com/ushatech/aestoreskotlin/ui/adapter/SimilarProductAdapter.kt
@@ -0,0 +1,40 @@
+package com.ushatech.aestoreskotlin.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ushatech.aestoreskotlin.R
+import com.ushatech.aestoreskotlin.data.ProductDetailResponseData
+import com.ushatech.aestoreskotlin.databinding.VariationItemBinding
+
+class SimilarProductAdapter(var context: Context,var data:ArrayList<ProductDetailResponseData.SimilarProducts>):RecyclerView.Adapter<SimilarProductAdapter.SimilarProductViewHolder>() {
+
+
+    lateinit var binding: VariationItemBinding
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimilarProductViewHolder {
+        binding = VariationItemBinding.inflate(LayoutInflater.from(context),parent,false)
+        return SimilarProductViewHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: SimilarProductViewHolder, position: Int) {
+        holder.binding.tvProductName.text = data.get(position).name
+        holder.binding.tvProductPrice.text = "Rs ${data.get(position).price}"
+        Glide.with(context).load(data.get(position).image).placeholder(R.drawable.ic_banner_home).into(holder.binding.ivProductImage)
+
+
+
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return data.size
+
+    }
+
+    class SimilarProductViewHolder(var binding:VariationItemBinding):RecyclerView.ViewHolder(binding.root)
+}
--- app/src/main/java/com/ushatech/aestoreskotlin/ui/fragments/ProductDetailFragment.kt
+++ app/src/main/java/com/ushatech/aestoreskotlin/ui/fragments/ProductDetailFragment.kt
@@ -1,17 +1,27 @@
 package com.ushatech.aestoreskotlin.ui.fragments
 
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.HttpException
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.ushatech.aestoreskotlin.R
 import com.ushatech.aestoreskotlin.base.BaseFragment
 import com.ushatech.aestoreskotlin.data.ProductDetailResponseData
@@ -23,10 +33,12 @@ import com.ushatech.aestoreskotlin.presentation.ProductDetailViewModel
 import com.ushatech.aestoreskotlin.session.AppSession
 import com.ushatech.aestoreskotlin.session.Constant
 import com.ushatech.aestoreskotlin.ui.adapter.ProductImageAdapter
+import com.ushatech.aestoreskotlin.ui.adapter.SimilarProductAdapter
 import com.ushatech.aestoreskotlin.uitls.FragmentUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.util.ArrayList
 
 
 private const val ARG_PARAM1 = "param1"
@@ -158,6 +170,14 @@ class ProductDetailFragment : BaseFragment(), ProductImageAdapter.onEvent {
 
 
         setupRatingBars(product)
+        setupYoutubeThumbnail(product.data?.youtubeurl.toString())
+        if(product.data?.similarProducts!=null){
+            if(!product.data?.similarProducts?.isEmpty()!!){
+                setupSimilarProducts(product.data!!.similarProducts)
+
+            }
+        }
+
 
 
 
@@ -181,6 +201,55 @@ class ProductDetailFragment : BaseFragment(), ProductImageAdapter.onEvent {
 
 
 
+    }
+
+    private fun setupSimilarProducts(similarProducts: ArrayList<ProductDetailResponseData.SimilarProducts>) {
+        binding.recvSimilarProduct.adapter = SimilarProductAdapter(fragmentContext,similarProducts)
+        binding.recvSimilarProduct.layoutManager = LinearLayoutManager(fragmentContext,LinearLayoutManager.HORIZONTAL,false)
+    }
+
+    private fun setupYoutubeThumbnail(videoId: String) {
+        try {
+            Glide.with(fragmentContext).load("${Constant.Youtube_image_Url}$videoId/default.jpg").placeholder(R.drawable.ic_banner_home).listener(object :
+                RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.ytThumbnaillayout.visibility = View.GONE
+                    return true
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    TODO("Not yet implemented")
+                }
+            }).into(binding.ivYoutubeThumbnail)
+
+        }catch (e:HttpException){
+            binding.ivYoutubeThumbnail.visibility = View.GONE
+        }
+
+        binding.ivYoutubeThumbnail.setOnClickListener {
+            // open youtube intent
+            val appIntent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:$id"))
+            val webIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("http://www.youtube.com/watch?v=$videoId")
+            )
+            try {
+                fragmentContext.startActivity(appIntent)
+            } catch (ex: ActivityNotFoundException) {
+                fragmentContext.startActivity(webIntent)
+            }
+        }
     }
 
     private fun setupRatingBars(product: ProductDetailResponseData) {
@@ -266,6 +335,9 @@ class ProductDetailFragment : BaseFragment(), ProductImageAdapter.onEvent {
 
 
         }
+
+
+
         binding.ivIncre.setOnClickListener {
             val currentQuantity = binding.tvQuantity.text.toString().toInt()
             if(currentQuantity>=1){
